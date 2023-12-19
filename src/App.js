8450440733691a8126dd948e8fd05202db26609b
PranavNerkar <pranavnerkar321@gmail.com>
2023-12-20 00:09:58 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Routes, Switch, Link } from 'react-router-dom';
+import NavigationBar from './Pages/Home/NavBar';
 
 function App() {
   return (
     <div>
-      
+      <NavigationBar />
+      {/* <Router>
+        <Routes>
+          <Route path="/" exact component={NavigationBar} />
+          <Route path="/Farmer-LogIn" component={<NavigationBar />} />
+        </Routes>
+      </Router> */}
     </div>
   );
 }
--- src/Pages/Home/NavBar.jsx
+++ src/Pages/Home/NavBar.jsx
@@ -1,11 +1,149 @@
-import react from 'react';
+import React, { useState } from "react";
+import logo from "./../../images/Home/logo.png";
+// import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
-function NavBar() {
-    return (
-        <div>
-            <nav className='nav'>
-            
-            </nav>
+const NavigationBar = () => {
+  const [flag1, setFlag1] = useState(false);
+  const [flag2, setFlag2] = useState(false);
+  const [flag3, setFlag3] = useState(false);
+  const [flag4, setFlag4] = useState(false);
+
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src={logo} height="42px" width="75px" />
+          </a>
+          <div class="navbar-brand me-5" style={{ color: "darkgreen" }}>
+            {" "}
+            <b>Farm2City</b>{" "}
+          </div>
+
+          <div
+            class="ml-50 collapse navbar-collapse "
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link navbar-brand"
+                  style={{ color: "darkgreen" }}
+                  aria-current="page"
+                  href="#"
+                  onMouseOver={() => {
+                    setFlag1(true);
+                  }}
+                  onMouseOut={() => {
+                    setFlag1(false);
+                  }}
+                >
+                  Information
+                </a>
+                <div
+                  style={{
+                    display: flag1 ? "block" : "none",
+                    height: "3px",
+                    width: "100%",
+                    backgroundColor: "darkgreen",
+                    borderRadius: "5%",
+                  }}
+                ></div>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link navbar-brand"
+                  style={{ color: "darkgreen" }}
+                  href="#"
+                  onMouseOver={() => {
+                    setFlag2(true);
+                  }}
+                  onMouseOut={() => {
+                    setFlag2(false);
+                  }}
+                >
+                  What-We-Provide
+                </a>
+                <div
+                  style={{
+                    display: flag2 ? "block" : "none",
+                    height: "3px",
+                    width: "100%",
+                    backgroundColor: "darkgreen",
+                    borderRadius: "5%",
+                  }}
+                ></div>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link navbar-brand"
+                  style={{ color: "darkgreen" }}
+                  href="#"
+                  onMouseOver={() => {
+                    setFlag3(true);
+                  }}
+                  onMouseOut={() => {
+                    setFlag3(false);
+                  }}
+                >
+                  Reviews
+                </a>
+                <div
+                  style={{
+                    display: flag3 ? "block" : "none",
+                    height: "3px",
+                    width: "100%",
+                    backgroundColor: "darkgreen",
+                    borderRadius: "5%",
+                  }}
+                ></div>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link navbar-brand"
+                  style={{ color: "darkgreen" }}
+                  href="#"
+                  onMouseOver={() => {
+                    setFlag4(true);
+                  }}
+                  onMouseOut={() => {
+                    setFlag4(false);
+                  }}
+                >
+                  Contact-Us
+                </a>
+                <div
+                  style={{
+                    display: flag4 ? "block" : "none",
+                    height: "3px",
+                    width: "100%",
+                    backgroundColor: "darkgreen",
+                    borderRadius: "5%",
+                  }}
+                ></div>
+              </li>
+            </ul>
+            <form class="d-flex">
+              {/* <Link to="/Farmer-LogIn" target="_blank">
+              </Link> */}
+                <button class="btn btn-outline-success me-4 btn-lg">
+                  Farmer
+                </button>
+
+              {/* <Link to="/Buyer-LogIn" target="_blank">
+              </Link> */}
+                <button class="btn btn-outline-success btn-lg me-4">
+                  Buyer
+                </button>
+
+              {/* <button class="btn btn-outline-success btn-lg me-4"></button> */}
+            </form>
+          </div>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
